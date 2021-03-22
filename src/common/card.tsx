9f694c62b3ca017fef936fed9a69706a008abe19
samodurov_alexander <samodurov_alexander@mail.ru>
2021-03-22 16:26:58 +0300

    add info to card

--- src/common/card.tsx
+++ src/common/card.tsx
@@ -25,11 +25,16 @@ function Card(props:any) {
   return (
     <div style={{border: '1px solid white', margin: '20px', width: '500px', display: 'grid', justifyContent:'center', textAlign: 'center'}}>
         <h1>{props.data.name ? props.data.name : 'no name'}</h1>
-        <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Error excepturi fugiat ipsam obcaecati porro!
-            Adipisci amet atque cum dolor illo inventore,
-            minima odit perspiciatis possimus quidem quis reiciendis, sapiente voluptatem.
-        </h2>
+        <h2>{props.data.gender ? 'gender:' + props.data.gender : null}</h2>
+        <h2>{props.data.culture ? 'culture:' + props.data.culture : null}</h2>
+        <h2>{props.data.born ? 'born:' + props.data.born : null}</h2>
+        <h2>{props.data.numberOfPages ? 'number of pages:' + props.data.numberOfPages : null}</h2>
+        <h2>{props.data.country ? 'country:' + props.data.country : null}</h2>
+        <h2>{props.data.mediaType ? 'media type:' + props.data.mediaType : null}</h2>
+        <h2>{props.data.region ? 'region:' + props.data.region : null}</h2>
+        <h2>{props.data.words ? 'words:' + props.data.words : null}</h2>
+        <h2>{props.data.coatOfArms ? 'coat of arms:' + props.data.coatOfArms : null}</h2>
+
         <div onClick={toggleFavorites}
              style={{fontSize: '30px', cursor: 'pointer'}}
         >{isFavorite ? checkedStar : uncheckedStar}</div>
